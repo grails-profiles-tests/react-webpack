379234a4919dde3c1bf628f238686a2ccd0ff435
Sergio del Amo <sergio.delamo@softamo.com>
2019-01-15 14:58:50 +0100

    Update profiles-install.sh

--- profiles-install.sh
+++ profiles-install.sh
@@ -39,6 +39,6 @@ sdk use grails dev
 
 grails --version
 
-grails create-app demo.react-webpack -profile=react-webpack
+grails create-app demo.react-webpack --profile=react-webpack
 
 exit $EXIT_STATUS
